bbcf40c2ef28549cf9947745c9491350005d3123
Karol Przybyszewski <karol.przyb@gmail.com>
2023-04-14 21:51:37 -0500

    Smoke generation working. Ingest looking good.

---
+++ src/libs/pyclouds/Config.py
@@ -0,0 +1,11 @@
+from time import time
+
+CloudHeight =  1024
+CloudWidth = 1024
+
+PixelSize = 2
+
+NoiseOffset = 0.4
+Seed = int(time())
+
+Framerate = 30
---
+++ src/train.py
@@ -0,0 +1,3 @@
+from ..libs import pyclouds
+
+pyclouds.PygameCore.
