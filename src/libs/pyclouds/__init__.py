9ae4cbb960f55e3f299eae460785493694c614e4
Karol Przybyszewski <karol.przyb@gmail.com>
2023-04-25 01:57:37 -0500

    synthetic smoke notebook done

--- src/libs/pyclouds/__init__.py
+++ src/libs/pyclouds/__init__.py
@@ -1,21 +1,25 @@
-HasPyglet = True
-HasPygame = True
+# HasPyglet = True
+# HasPygame = True
 
-try:
-	import pyglet
-except ImportError:
-	HasPyglet = False
+# try:
+# 	import pyglet
+# except ImportError:
+# 	HasPyglet = False
 
-try:
-	import pygame
-except ImportError:
-	HasPygame = False
+# try:
+# 	import pygame
+# except ImportError:
+# 	HasPygame = False
 
-if HasPyglet:
-	print("Using Pyglet")
-	import PygletCore
-elif HasPygame:
-	print("Using Pygame")
-	from .PygameCore import *
-else:
-	print("Install Pyglet or Pygame to run")
+# if HasPyglet:
+# 	print("Using Pyglet")
+# 	import PygletCore
+# elif HasPygame:
+# 	print("Using Pygame")
+# 	from .PygameCore import *
+# else:
+# 	print("Install Pyglet or Pygame to run")
+
+# import noise
+# import perlin
+# import Config
