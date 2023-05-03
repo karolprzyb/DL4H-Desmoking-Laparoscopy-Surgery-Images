3c9c567dacece3fce4f76160d51bf387c858c508
Karol Przybyszewski <karol.przyb@gmail.com>
2023-05-03 02:06:39 -0500

    Complete dark channel applied in trainer.

--- src/SynthTrainer.py
+++ src/SynthTrainer.py
@@ -10,8 +10,9 @@ from data_augmentation import Trainer
 
 from losses import PSNR
 from losses import SSIMLoss
-#from libs.pytorch_fsim.fsim import FSIMc
-from torchmetrics.functional import universal_image_quality_index
+from losses import UQILoss
+from losses import FSIMcpiq
+from dcpref import DarkChannelPrior
 
 class SynthTrainer(Trainer):
     '''
@@ -27,7 +28,7 @@ class SynthTrainer(Trainer):
                  training_criterion:torch.nn.Module, 
                  writer:SummaryWriter,
                  optimizer:Optimizer,
-                 val_crit_list:List[torch.nn.Module] = [PSNR(2.0), SSIMLoss(2.0), universal_image_quality_index], # [PSNR(2.0), torch.nn.MSELoss(), torch.nn.L1Loss()]
+                 val_crit_list:List[torch.nn.Module] = [PSNR(2.0), SSIMLoss(2.0), UQILoss((-1,1)), FSIMcpiq([-1,1])], # [PSNR(2.0), torch.nn.MSELoss(), torch.nn.L1Loss()]   # THIS IS TOO SLOW using piq -- FSIMcLoss((-1.0,1.0), 256, 1)
                  single_frame:bool = False, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.device = device
@@ -66,6 +67,7 @@ class SynthTrainer(Trainer):
         self.true_mask_list:List[torch.Tensor] = []
         self.bg_pred_list:List[torch.Tensor] = []
         self.mask_pred_list:List[torch.Tensor] = []
+        self.dark_channel_list:List[torch.Tensor] = []
         self.val_indices_to_show = np.linspace(0, len(validation_set), 5, endpoint=False).astype(int)  # type: ignore
         
         super().validation(net, validation_set, *args, **kwargs)
@@ -99,6 +101,8 @@ class SynthTrainer(Trainer):
             self.writer.add_images('Synthetic: Smoke Mask', torch.stack(self.true_mask_list),  self.global_step)
         if self.mask_pred_list:
             self.writer.add_images('Synthetic: Predicted Smoke Mask', torch.stack(self.mask_pred_list), self.global_step)
+        if self.dark_channel_list:
+            self.writer.add_images('Synthetic: Dark Channel Mask', torch.stack(self.dark_channel_list), self.global_step)
 
     def validationIteration(self, net: torch.nn.Module, dataset_tuple: Tuple, *args, **kwargs):
         smoke_img, true_mask, bg_img = dataset_tuple
@@ -109,6 +113,9 @@ class SynthTrainer(Trainer):
         # Compute validation loss and save statistic (total)
         for idx, val_crit in enumerate(self.val_crit_list):
             if bg_pred is not None:
+                # temp = val_crit(bg_pred, bg_img.to(bg_pred.device)).item()
+                # print(type(temp))
+                # print(val_crit.__class__.__name__)
                 self.total_validation_loss[idx] += val_crit(bg_pred, bg_img.to(bg_pred.device)).item()
                 self.total_validation_reference[idx] += val_crit(smoke_img.to(bg_pred.device), bg_img.to(bg_pred.device)).item()
 
@@ -124,6 +131,8 @@ class SynthTrainer(Trainer):
             if bg_pred is not None:
                 bg_pred = ( (bg_pred[0].cpu() + 1) / 2 )
                 self.bg_pred_list.append(bg_pred)
+            if self.use_dark_channel:
+                self.dark_channel_list.append(self.dcp_gen(smoke_img.to(self.device).unsqueeze(0))[0].cpu())
 
     def testIteration(self, net: torch.nn.Module, dataset_tuple: Tuple, *args, **kwargs):
         smoke_img, true_mask, bg_img = dataset_tuple
@@ -134,38 +143,41 @@ class SynthTrainer(Trainer):
         # Compute validation loss and save statistic (total)
         for idx, val_crit in enumerate(self.val_crit_list):
             if bg_pred is not None:
-                self.total_validation_loss[idx] += val_crit(bg_pred, bg_img.to(bg_pred.device)).item()
-                self.total_validation_reference[idx] += val_crit(smoke_img.to(bg_pred.device), bg_img.to(bg_pred.device)).item()
+                self.total_validation_loss_test[idx] += val_crit(bg_pred, bg_img.to(bg_pred.device)).item()
+                self.total_validation_reference_test[idx] += val_crit(smoke_img.to(bg_pred.device), bg_img.to(bg_pred.device)).item()
 
         # Save images to show in tensorboard
-        if self.validation_step in self.val_indices_to_show:
+        if self.test_step in self.test_indices_to_show:
             bg_img =  ( (bg_img[0].cpu() + 1) / 2)
             smoke_img = ( (smoke_img[0].cpu() + 1) / 2)
             true_mask = ( (true_mask[0].cpu() + 1) / 2 )
-            self.bg_img_list.append(bg_img)
-            self.smoke_img_list.append(smoke_img[:3, : ,:])
-            self.true_mask_list.append(true_mask[0,  : ,:].unsqueeze(0))
+            self.bg_img_list_test.append(bg_img)
+            self.smoke_img_list_test.append(smoke_img[:3, : ,:])
+            self.true_mask_list_test.append(true_mask[0,  : ,:].unsqueeze(0))
 
             if bg_pred is not None:
                 bg_pred = ( (bg_pred[0].cpu() + 1) / 2 )
-                self.bg_pred_list.append(bg_pred)
+                self.bg_pred_list_test.append(bg_pred)
+            if self.use_dark_channel:
+                self.dark_channel_list_test.append(self.dcp_gen(smoke_img.to(self.device).unsqueeze(0))[0].cpu())
 
     def test(self, net:torch.nn.Module, test_set:Dataset, *args, **kwargs):
         '''
             Wrapper of test validation function to save validation losses to tensorboard
         '''
          # Keeps track of validation losses. See validationIteration for how this is used
-        self.total_validation_loss = [0 for _ in range(len(self.val_crit_list))]
-        self.total_validation_reference = [0 for _ in range(len(self.val_crit_list))]
+        self.total_validation_loss_test = [0 for _ in range(len(self.val_crit_list))]
+        self.total_validation_reference_test = [0 for _ in range(len(self.val_crit_list))]
 
         # Keeps track of images to show in tensorboard.
-        # Images according to val_indices_to_show should be filled in networkInference function 
-        self.bg_img_list:List[torch.Tensor] = []
-        self.smoke_img_list:List[torch.Tensor] = []
-        self.true_mask_list:List[torch.Tensor] = []
-        self.bg_pred_list:List[torch.Tensor] = []
-        self.mask_pred_list:List[torch.Tensor] = []
-        self.val_indices_to_show = np.linspace(0, len(test_set), 5, endpoint=False).astype(int)  # type: ignore
+        # Images according to test_indices_to_show should be filled in networkInference function 
+        self.bg_img_list_test:List[torch.Tensor] = []
+        self.smoke_img_list_test:List[torch.Tensor] = []
+        self.true_mask_list_test:List[torch.Tensor] = []
+        self.bg_pred_list_test:List[torch.Tensor] = []
+        self.mask_pred_list_test:List[torch.Tensor] = []
+        self.dark_channel_list_test:List[torch.Tensor] = []
+        self.test_indices_to_show = np.linspace(0, len(test_set), 5, endpoint=False).astype(int)  # type: ignore
         
         super().test(net, test_set, *args, **kwargs)
 
@@ -173,8 +185,8 @@ class SynthTrainer(Trainer):
         self.writer.add_scalar('epoch_test', self.epoch_step, self.global_step)
 
         # Save validation losses to tensorboard
-        for val_crit, val_loss, val_loss_ref in zip(self.val_crit_list, self.total_validation_loss, 
-                                                        self.total_validation_reference):
+        for val_crit, val_loss, val_loss_ref in zip(self.val_crit_list, self.total_validation_loss_test, 
+                                                        self.total_validation_reference_test):
             if val_loss != 0:
                 self.writer.add_scalar('TEST: Validation Loss - {}'.format(val_crit.__class__.__name__), 
                                         val_loss/len(test_set),   # type: ignore
@@ -188,13 +200,15 @@ class SynthTrainer(Trainer):
                                         self.global_step)
 
         # Save images to tensorboard
-        if self.bg_img_list:
-            self.writer.add_images('TEST: Raw Img', torch.stack(self.bg_img_list), self.global_step)
-        if self.smoke_img_list:
-            self.writer.add_images('TEST: Composed Img', torch.stack(self.smoke_img_list), self.global_step)
-        if self.bg_pred_list:
-            self.writer.add_images('TEST: Predicted Desmoked', torch.stack(self.bg_pred_list), self.global_step)
-        if self.true_mask_list:
-            self.writer.add_images('TEST: Smoke Mask', torch.stack(self.true_mask_list),  self.global_step)
-        if self.mask_pred_list:
-            self.writer.add_images('TEST: Predicted Smoke Mask', torch.stack(self.mask_pred_list), self.global_step)
+        if self.bg_img_list_test:
+            self.writer.add_images('TEST: Raw Img', torch.stack(self.bg_img_list_test), self.global_step)
+        if self.smoke_img_list_test:
+            self.writer.add_images('TEST: Composed Img', torch.stack(self.smoke_img_list_test), self.global_step)
+        if self.bg_pred_list_test:
+            self.writer.add_images('TEST: Predicted Desmoked', torch.stack(self.bg_pred_list_test), self.global_step)
+        if self.true_mask_list_test:
+            self.writer.add_images('TEST: Smoke Mask', torch.stack(self.true_mask_list_test),  self.global_step)
+        if self.mask_pred_list_test:
+            self.writer.add_images('TEST: Predicted Smoke Mask', torch.stack(self.mask_pred_list_test), self.global_step)
+        if self.dark_channel_list_test:
+            self.writer.add_images('TEST: Dark Channel Mask', torch.stack(self.dark_channel_list_test), self.global_step)
